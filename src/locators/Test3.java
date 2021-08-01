6f0766103193dc131dad35e2a8a7943121c575a4
QSP <QSP@QSP>
2021-08-01 10:08:21 +0530

    Locators

---
+++ src/locators/Test3.java
@@ -0,0 +1,39 @@
+package locators;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test3 {
+
+	public static void main(String[] args) {
+     WebDriver driver=new ChromeDriver();
+     driver.manage().window().maximize();
+     driver.get("https://demo.actitime.com/login.do");
+     
+     driver.findElement(By.id("username")).sendKeys("admin");
+     driver.findElement(By.name("pwd")).sendKeys("manager");
+     driver.findElement(By.xpath("//div[text()='Login ']")).click();
+     
+     String title = driver.getTitle();
+     System.out.println(title);
+     
+     String url = driver.getCurrentUrl();
+     System.out.println(url);
+     
+     if(title.equals("actiTIME - Login")) {
+    	 System.out.println("Pass:Home page is dispalyed");
+     }
+     
+     else
+     {
+    	 System.out.println("Fail:Home page is not dispalyed");
+     }
+     
+     
+
+     driver.close();
+
+	}
+
+}
---
+++ src/locators/Test4.java
@@ -0,0 +1,37 @@
+package locators;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test4 {
+
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver = new ChromeDriver();
+       driver.manage().window().maximize();
+       driver.get("https://www.instagram.com/accounts/login/");
+       
+       Thread.sleep(5000);
+       
+       driver.findElement(By.name("username")).sendKeys("ghgrhggggfg");
+       driver.findElement(By.name("password")).sendKeys("fhghgfhg");
+       driver.findElement(By.xpath("//div[text()='Log In']")).click();
+       
+       String title = driver.getTitle();
+       System.out.println(title);
+       
+      String url = driver.getCurrentUrl();
+      System.out.println(url);
+       
+       if(title.equals("Login • Instagram")) {
+    	   System.out.println("pass:home page is dispalyed");
+       }
+       else
+       {
+    	   System.out.println("Fail:home page is not dispalayed");
+       }
+       
+       driver.close();
+	}
+
+}
